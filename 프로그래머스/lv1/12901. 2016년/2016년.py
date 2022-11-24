5e3288c61565d1123e413959ede55a0bb385fc10
KD Park <62131378+Ramdatech@users.noreply.github.com>
2022-11-25 06:46:19 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -1,5 +1,3 @@
 def solution(a, b):
-    day = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    d = ['THU','FRI','SAT','SUN','MON','TUE','WED']
-    answer = d[(sum(day[:a]) + b)%7]
+    answer = ''
     return answer
