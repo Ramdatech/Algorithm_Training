5e6a9cfd6cd2de6671daa8bdb0a6b4e95b3e16b4
KD Park <62131378+Ramdatech@users.noreply.github.com>
2022-11-26 00:59:44 +0900

    [level 2] Title: 올바른 괄호, Time: 11.71 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = [0, 0] 
+    for i in s[::-1]:
+        answer[i==")"] += 1
+        if answer[0] > answer[1]:
+            return False
+    if answer[0] == answer[1] : 
+        return True
+    else : 
+        return False
