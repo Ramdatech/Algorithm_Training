32defa436fed75ff70c096a7cc1c4d7491a3bed4
KD Park <62131378+Ramdatech@users.noreply.github.com>
2022-11-25 09:08:43 +0900

    [level 1] Title: 최소직사각형, Time: 3.44 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,12 +1,5 @@
 def solution(sizes):
     answer = []
-    res = sizes[:]
+    res = [[x,y] if x>=y else [y,x] for x,y in sizes]
     a,b = list(zip(*res))
-    a = list(a)
-    b = list(b)
-    answer.append(max(a)*max(b))
-    for i in range(len(a)) :
-        t = b.index(max(b))
-        a[t], b[t] = b[t], a[t]
-        answer.append(max(a)*max(b))
-    return min(answer)
+    return max(a)*max(b)
