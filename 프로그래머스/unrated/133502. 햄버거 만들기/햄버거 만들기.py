8ff1afb100de1004ea754d081570c40b6f2258ff
KD Park <62131378+Ramdatech@users.noreply.github.com>
2022-11-26 08:00:20 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -3,9 +3,7 @@ def solution(ingredient):
     burger = []
     for i in ingredient :
         if burger[-3:]+[i] == [1,2,3,1]  :
-            burger.pop(-1)
-            burger.pop(-1)
-            burger.pop(-1)
+            for x in range(3) : burger.pop(-1)
             answer += 1
         else :
             burger.append(i)
