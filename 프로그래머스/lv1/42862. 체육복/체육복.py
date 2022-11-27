b5b908dc22d44a861018ce92dc674cb416a527c8
KD Park <62131378+Ramdatech@users.noreply.github.com>
2022-11-28 00:39:25 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -0,0 +1,23 @@
+def solution(n, lost, reserve):
+    clothe = [1] * n
+    for i in reserve :
+        clothe[i-1] += 1
+    for i in lost :
+        clothe[i-1] -= 1
+    answer = []
+    for i in range(len(clothe)) :
+        var = clothe.pop(-1)
+        if i == 0 : 
+            answer.append(var)
+            continue
+        if var == 2 and answer[-1] == 0 :
+            answer[-1] = 1
+            answer.append(1)
+        elif var == 0 and answer[-1] == 2 :
+            answer[-1] = 1
+            answer.append(1)
+        else :
+            answer.append(var)
+            
+            
+    return len(answer)-answer.count(0)
