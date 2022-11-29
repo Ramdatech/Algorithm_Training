a110cef7f1d085f085c3d71dec4dc51cccdb03fc
KD Park <62131378+Ramdatech@users.noreply.github.com>
2022-11-30 06:51:40 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,12 +1,2 @@
 def solution(n):
-    ans = 0
-    res = n
-    while 1 :
-        if res % 2 == 0 :
-            res /= 2
-        else :
-            res -= 1
-            ans += 1
-        if res == 0:
-            break
-    return ans
+    return bin(n).count('1')
