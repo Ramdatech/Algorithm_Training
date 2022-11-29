a93d0f0a472a13c8f8e9ef7a1228be57248ec1bd
KD Park <62131378+Ramdatech@users.noreply.github.com>
2022-11-30 08:11:04 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,10 +1,10 @@
 import math
 def solution(progresses, speeds):
     answer = []
-    ls = [math.ceil((100-x[0])/x[1]) for x in zip(progresses, speeds)]
     mx = -1
     count = 0
-    for x in ls :
+    for i in zip(progresses, speeds) :
+        x = math.ceil((100-i[0])/i[1])
         if x > mx : 
             mx = x
             answer.append(0)
