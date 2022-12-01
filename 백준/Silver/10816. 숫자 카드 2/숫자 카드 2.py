54ae846758b60f86fab624d8733036945feeb571
KD Park <62131378+Ramdatech@users.noreply.github.com>
2022-12-01 12:32:34 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 840 ms, Memory: 128948 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,26 +1,8 @@
 import sys
+from collections import Counter
 input = sys.stdin.readline
 n = int(input().strip())
-ls = list(map(int,input().strip().split()))
+ls = Counter(list(map(int,input().strip().split())))
 m = int(input().strip())
-ls2 = list(map(int,input().strip().split()))
-ls.sort()
-answer = []
-dicty = {}
-for goal in sorted(list(set(ls))):
-    start, end = 0, n-1
-    while 1 :
-        mid = (start+end)//2
-        if ls[mid] >= goal :
-            end = mid
-        elif ls[mid] < goal :
-            start = mid+1
-        if start >= end :
-            answer.append([ls[end], n-end])
-            break
-for i in range(1, len(answer)) :
-    dicty[answer[i-1][0]] = answer[i-1][1] - answer[i][1]
-else :
-    dicty[answer[-1][0]] = answer[-1][1]
-for i in ls2:
-    print(dicty.get(i,0), end=" ")
+for i in list(map(int,input().strip().split())):
+    print(ls.get(i,0), end=' ')
