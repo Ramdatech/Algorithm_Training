87d2bc51f04dcd4887cf67f863912d4109b0bbcb
KD Park <62131378+Ramdatech@users.noreply.github.com>
2022-12-02 06:29:03 +0900

    [Gold V] Title: 리모컨, Time: 1384 ms, Memory: 69808 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -5,15 +5,15 @@ n = int(input().strip())
 if n == 0 :
     ls = []
 else :
-    ls = list(map(int,input().split()))
+    ls = list(input().split())
 
 answer = -1
 result = [abs(100-channel)]
 while 1:
     answer += 1
-    if len(set(map(int,str(channel + answer))) & set(ls)) == 0 :
+    if set(str(channel + answer)) & set(ls) == set() :
         result.append(answer + len(str(channel+answer)))
-    if channel-answer >= 0 and (len(set(map(int,str(channel - answer))) & set(ls)) == 0) :
+    if channel-answer >= 0 and set(str(channel - answer)) & set(ls) == set() :
         result.append(answer + len(str(channel-answer)))
     if answer > abs(100-channel):
         break
