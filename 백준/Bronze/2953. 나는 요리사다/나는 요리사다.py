7a76ac23bf8509c752393ca80a883c5ec491adb4
KD Park <62131378+Ramdatech@users.noreply.github.com>
2022-12-06 18:08:30 +0900

    [Bronze III] Title: 나는 요리사다, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2953. 나는 요리사다/나는 요리사다.py
+++ 백준/Bronze/2953. 나는 요리사다/나는 요리사다.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 res = [0, 0]
 for idx in range(5):
-    ls = list(map(int,input().split()))
-    if sum(ls) > res[1] :
-        res = [idx+1, sum(ls)]
+    ls = sum(list(map(int,input().split())))
+    if ls > res[1] :
+        res = [idx+1, ls]
 print(*res)
