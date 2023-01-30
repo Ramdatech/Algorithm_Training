b14d1fca2228782894d7c1b186327c2bbcf4bd61
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-01-30 19:03:01 +0900

    [Gold V] Title: 암호 만들기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,13 +1,10 @@
 import sys
 from itertools import combinations, permutations
-
 input = sys.stdin.readline
 a, b = map(int, input().split())
 ls = sorted(input().split())
+t = set(['a', 'e', 'i', 'o', 'u'])
 res = []
 for i in sorted(combinations(ls, a)):
-    if len(set(i) & set(['a', 'e', 'i', 'o', 'u'])) >= 1 and len(set(i)-set(['a', 'e', 'i', 'o', 'u'])) >= 2:
-        res.append(''.join(i))
-res.sort()
-for i in res :
-    print(i)
+    if len(set(i) & t) >= 1 and len(set(i)-t) >= 2:
+        print(''.join(i))
