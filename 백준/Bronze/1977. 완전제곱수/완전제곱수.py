23f1cacb46569091e06cc7a2c52e43db7eccfc12
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-01-31 07:47:49 +0900

    [Bronze II] Title: 완전제곱수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1977. 완전제곱수/완전제곱수.py
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.py
@@ -1,7 +1,7 @@
-import sys, math
+import sys
 input = sys.stdin.readline
 a, b = [int(input()) for _ in [1,2]]
-res = [x**2 for x in range(math.ceil(a**0.5), math.ceil(b**0.5)+1) if a<=x**2<=b]
+res = [x for x in range(a, b+1) if round(x**0.5) == x**0.5]
 if res :
     print(sum(res))
     print(res[0])
