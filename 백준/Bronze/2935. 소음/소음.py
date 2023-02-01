8b746dfb4cc42249b2f4abd7fb44b81b28e715ff
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-02-01 10:57:20 +0900

    [Bronze III] Title: 소음, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2935. 소음/소음.py
+++ 백준/Bronze/2935. 소음/소음.py
@@ -1,10 +1,1 @@
-import sys
-input = sys.stdin.readline
-ls = sorted([input().strip() for x in range(3)] , key = lambda x: (len(x), x.isdecimal()))
-o = ls.pop(0)
-if o == "*" :
-    print("1" + "0"*(sum([len(x)-1 for x in ls])))
-else:
-    res = list(map(int, list(ls[1])))
-    res[-1*len(ls[0])] += 1
-    print(''.join(map(str,res)))
+print(eval(''.join(input()for x in range(3))))
