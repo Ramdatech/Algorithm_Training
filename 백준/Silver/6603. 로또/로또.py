6961ee4a029102b5e69931a0da31fe965fdd13fd
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-02-01 15:32:38 +0900

    [Silver II] Title: 로또, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -4,7 +4,6 @@ for input in sys.stdin :
     a = list(map(int, input.split()))
     if a == [0] :
         break
-    # print(a)
     b = list(combinations(a[1:], 6))
     for i in b :
         print(*sorted(list(i)))
