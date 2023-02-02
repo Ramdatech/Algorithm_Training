b03722474af29ba178bbd2b778bfa37ef1ea3f0a
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-02-02 20:48:07 +0900

    [Silver III] Title: N과 M (2), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -2,5 +2,4 @@ import sys
 from itertools import combinations
 t = sys.stdin.readline
 a, b = map(int, t().split())
-for i in combinations([x for x in range(1, a+1)], b) :
-    print(*i)
+print(*[' '.join(i) for i in combinations([str(x) for x in range(1, a+1)], b)], sep='\n')
