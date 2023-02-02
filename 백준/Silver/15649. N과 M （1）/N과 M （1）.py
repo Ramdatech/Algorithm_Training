2d4e4f2f606246a258fe99158610f59139d00e3f
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-02-02 20:47:16 +0900

    [Silver III] Title: N과 M (1), Time: 48 ms, Memory: 34276 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -2,4 +2,4 @@ import sys
 from itertools import permutations
 t = sys.stdin.readline
 a, b = map(int, t().split())
-print(*[' '.join(i) for i in permutations([str(x) for x in range(1, a+1)], b)], sep='\n')
+print('\n'.join([' '.join(i) for i in permutations([str(x) for x in range(1, a+1)], b)]))
