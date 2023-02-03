04dcf57fbf6ffd6d75ad2796ca6da73aa1e09a3e
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-02-03 16:46:36 +0900

    [Silver IV] Title: 최대 GCD, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9417. 최대 GCD/최대 GCD.py
+++ 백준/Silver/9417. 최대 GCD/최대 GCD.py
@@ -1,22 +1,19 @@
 import sys
-
-def get_gcd(num1, num2):
-    x, y = min(num1, num2), max(num1, num2)
+def gcd(num1, num2):
+    x, y = sorted([num1, num2])
     while 1 :
         if x%y == 0:
             return y
         else :
             x, y = y, x%y
-    
-
 t = sys.stdin.readline
 n = int(t())
 for _ in range(n):
-    res = None
+    res = -1
     ls = list(map(int,t().split()))
     for i in range(len(ls)):
         for j in range(i+1, len(ls)):
-            temp = get_gcd(ls[i], ls[j])
-            if res == None or temp > res:
+            temp = gcd(ls[i], ls[j])
+            if temp > res:
                 res = temp
     print(res)
