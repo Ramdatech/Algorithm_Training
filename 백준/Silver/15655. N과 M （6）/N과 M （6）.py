7eb2081f13b27cd0824484406c1b0c305f45f0ed
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-02-03 11:27:14 +0900

    [Silver III] Title: N과 M (6), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -2,5 +2,4 @@ import sys
 from itertools import combinations
 t = sys.stdin.readline
 a, b = map(int, t().split())
-a = map(str,sorted(list(map(int, t().split()))))
-print('\n'.join([' '.join(i) for i in combinations(a, b)]))
+print('\n'.join([' '.join(i) for i in combinations(map(str,sorted(list(map(int, t().split())))), b)]))
