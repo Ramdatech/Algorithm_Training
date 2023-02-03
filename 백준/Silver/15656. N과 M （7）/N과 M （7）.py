ce526ce8fb42ef1e48e66520e458b3dd890db2dd
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-02-03 11:26:35 +0900

    [Silver III] Title: N과 M (7), Time: 296 ms, Memory: 138044 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.py
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -2,5 +2,4 @@ import sys
 from itertools import product
 t = sys.stdin.readline
 a, b = map(int, t().split())
-a = map(str,sorted(list(map(int, t().split()))))
-print('\n'.join([' '.join(i) for i in product(a, repeat=b)]))
+print('\n'.join([' '.join(i) for i in product(map(str,sorted(list(map(int, t().split())))), repeat=b)]))
