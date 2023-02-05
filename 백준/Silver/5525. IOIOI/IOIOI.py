527e6d3d056e6f9868ad6b8b807c72801d988b68
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-02-05 09:48:01 +0900

    [Silver I] Title: IOIOI, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -5,7 +5,11 @@ P = "I" + "OI"*n
 lenS = int(t())
 S = t().strip()
 res = 0
-for i in range(lenS-len(P)+1):
+jump = 1
+for i in range(0, lenS-len(P)+1, jump):
     if S[i:i+len(P)] == P :
         res += 1
+        jump = 2
+    else :
+        jump = 1
 print(res)
