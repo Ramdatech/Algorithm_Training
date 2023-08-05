0c46a4addade47653103d42ff10042e301f4024f
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-08-05 20:33:46 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -1,12 +1,7 @@
 import math
-n = input()
-a = map(int, input().split())
-res = [0, 0]
+n, a, r = input(), map(int, input().split()), [0, 0]
 for i in a :
-    res = [res[j] + math.ceil(i/(30*(j+1)) if i/(30*(j+1))!=int(i/(30*(j+1))) else i/(30*(j+1))+1) * (5*(j+2)) for j in range(2)]
-if res[0] < res[1] :
-    print("Y", res[0])
-elif res[0] == res[1] :
-    print("Y M", res[0])
-else :
-    print("M", res[1])
+    r = [r[j-1] + math.ceil(i/(30*j) if i/(30*j)!=int(i/(30*j)) else i/(30*j)+1) * (5*(j+1)) for j in range(1,3)]
+if r[0] < r[1] : print("Y", r[0])
+elif r[0] > r[1] : print("M", r[1])
+else : print("Y M", r[0])
