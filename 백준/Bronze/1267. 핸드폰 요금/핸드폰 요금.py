7d7c28560fec86e0e96418b34df38ab30b3d866b
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-08-05 20:41:14 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -1,7 +1,7 @@
-import math
 n, a, r = input(), map(int, input().split()), [0, 0]
 for i in a :
-    r = [r[j-1] + math.ceil(i/(30*j) if i/(30*j)!=int(i/(30*j)) else i/(30*j)+1) * (5*(j+1)) for j in range(1,3)]
-if r[0] < r[1] : print("Y", r[0])
-elif r[0] > r[1] : print("M", r[1])
-else : print("Y M", r[0])
+    r = [r[j-1] + (i//(30*j)+1) * (5*(j+1)) for j in range(1,3)]
+x, y = r
+if x < y : print("Y", x)
+elif x > y : print("M", y)
+else : print("Y M", x)
