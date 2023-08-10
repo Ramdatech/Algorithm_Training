d5a00a1f072cf2685597b9359061a2cdd1d9bdd3
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-08-11 03:18:11 +0900

    [Silver III] Title: 수열, Time: 84 ms, Memory: 40292 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.py
@@ -0,0 +1,11 @@
+import sys
+t = sys.stdin.readline
+a, b = map(int, t().split())
+ls = list(map(int, t().split()))
+s, e = 0, b
+res = [sum(ls[s:e])]
+for i in range(a-b) : 
+    res.append(res[-1]-ls[s]+ls[e])
+    s+=1
+    e+=1
+print(max(res))
