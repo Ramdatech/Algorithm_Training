1a47240b685ae02370cbb56c39c5cb769ce8688a
KD Park <62131378+Ramdatech@users.noreply.github.com>
2023-08-11 03:22:06 +0900

    [Silver III] Title: 수열, Time: 84 ms, Memory: 40292 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,9 +1,10 @@
-a, b = map(int, input().split())
-ls = list(map(int, input().split()))
+def f():
+    return map(int, input().split())
+a, b = f()
+ls = list(f())
 s, e = 0, b
 res = [sum(ls[s:e])]
 for i in range(a-b) : 
     res.append(res[-1]-ls[s]+ls[e])
-    s+=1
-    e+=1
+    s+=1; e+=1
 print(max(res))
