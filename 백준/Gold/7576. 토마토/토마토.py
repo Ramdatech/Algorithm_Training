14cbb823abe7f153469a039a5fc6ecaa09bb26bf
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-05-12 14:23:43 +0900

    [Gold V] Title: 토마토, Time: 4060 ms, Memory: 302064 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -5,6 +5,7 @@ t = sys.stdin.readline
 m, n = map(int, t().strip().split())
 ls = [list(map(int, t().strip().split())) for _ in range(n)]
 pos = [(i, j, 0) for i in range(n) for j in range(m) if ls[i][j] == 1]
+zero = set([(i, j) for i in range(n) for j in range(m) if ls[i][j] == 0])
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
@@ -16,13 +17,14 @@ while que :
     if res < d : res = d
     if (x, y) in vst : continue
     vst.add((x, y))
+    zero.discard((x, y))
     for i in range(4) :
         nx, ny = x + dx[i], y + dy[i]
         if 0 <= nx < n and 0 <= ny < m and ls[nx][ny] == 0 :
             ls[nx][ny] = 1
             que.append((nx, ny, d+1))
 
-if sum(ls, []).count(0) > 0 :
+if zero :
     print(-1)
 else :
     print(res)
