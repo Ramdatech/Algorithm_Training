cbb1b2503e2feba024aea37355111623c1b7ed7b
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-05-14 10:28:25 +0900

    [Silver I] Title: 절댓값 힙, Time: 148 ms, Memory: 39824 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -5,10 +5,9 @@ res = []
 for _ in range(n) :
     tmp = int(t().strip())
     if tmp != 0 :
-        heapq.heappush(res, (abs(tmp), int(tmp/abs(tmp))))
+        heapq.heappush(res, (abs(tmp), tmp))
         continue
     if res :
-        a = heapq.heappop(res)
-        print(a[0] * a[1])
+        print(heapq.heappop(res)[1])
     else :
         print(0)
