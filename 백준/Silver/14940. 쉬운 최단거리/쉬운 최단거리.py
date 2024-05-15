95ce11c37dc6797e71e7cba8a77fac8d675fdc0a
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-05-15 16:15:47 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 896 ms, Memory: 69888 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -2,8 +2,13 @@ import sys
 from collections import deque
 t = sys.stdin.readline
 n, m = map(int, t().strip().split())
-ls = [list(map(int, t().strip().split())) for _ in range(n)]
-stt = [(x, y, 0) for x in range(n) for y in range(m) if ls[x][y] == 2]
+ls = []
+stt = []
+for i in range(n) :
+    tmp = list(map(int, t().strip().replace("1", "-1").split()))
+    ls.append(tmp)
+    if stt == [] and 2 in tmp:
+        stt.append((i, tmp.index(2), 0))
 dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 que = deque(stt)
@@ -16,13 +21,8 @@ while que :
 
     for dx, dy in dirs :
         nx, ny = x+dx, y+dy
-        if 0 <= nx < n and 0 <= ny < m and ls[nx][ny] == 1 :
+        if 0 <= nx < n and 0 <= ny < m and ls[nx][ny] == -1 :
             que.append((nx, ny, cnt+1))
-
-for i in range(n) :
-    for j in range(m) :
-        if (i, j) not in vst and ls[i][j] == 1 :
-            ls[i][j] = -1
 
 for i in ls :
     print(*i)
