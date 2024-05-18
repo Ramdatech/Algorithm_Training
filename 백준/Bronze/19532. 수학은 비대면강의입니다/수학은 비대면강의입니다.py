f9b9b79fe3dd72a80f56ad90d65d7f48f9d37a33
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-05-19 01:49:28 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,4 +1,3 @@
-import sys
-t = sys.stdin.readline
-a, b, c, d, e, f = map(int, t().strip().split())
-print((c*e-f*b)//(a*e-b*d), (c*d-f*a)//(b*d-a*e))
+a, b, c, d, e, f = map(int, input().split())
+n = (a*e-b*d)
+print((c*e-f*b)//n, (f*a-c*d)//n)
