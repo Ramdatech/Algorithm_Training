a391474af3d817320424826e94938a089910a29f
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-05-19 11:07:15 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 100 ms, Memory: 74428 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -1,26 +1,16 @@
+import math
 n = int(input())
 n = n//3
-res = 0
-while n % 2 == 0:
-    n = n//2
-    res += 1
+res = int(math.log2(n))
 
 def recurse(n):
     if n == 0 :
-        a = []
-        a += [" "*2 + "*" + " "*2]
-        a += [" " + "*" + " " + "*" + " "]
-        a += ["*"*5]
-        return a
+        return [" "*2 + "*" + " "*2, " " + "*" + " " + "*" + " ", "*"*5]
 
     a = recurse(n-1)
 
-    b = []
-    for i in a:
-        b.append(" "*(3 * 2**(n-1)) + i + " "*(3 * 2**(n-1)))
-    for i in a:
-        b.append(i + " " + i)
+    b = [" "*(3 * 2**(n-1)) + i + " "*(3 * 2**(n-1)) for i in a]
+    b += [i + " " + i for i in a]
     return b
-
 
 print("\n".join(recurse(res)))
