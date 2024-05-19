6eebcf633fbe66ac7429be01c7657425b8ae51da
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-05-19 14:02:15 +0900

    [Gold IV] Title: 최단경로, Time: 700 ms, Memory: 70544 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,28 @@
+import sys, heapq
+from collections import defaultdict
+t = sys.stdin.readline
+n, m = map(int, t().strip().split())
+stt = int(t().strip())
+graph = defaultdict(list)
+for _ in range(m) :
+    a, b, c = map(int, t().strip().split())
+    graph[a].append((b, c))
+    # graph[b].append((a, c))
+
+que = []
+heapq.heappush(que, (0, stt))
+dist = [1e12] * (n+1)
+while que :
+    dis, x = heapq.heappop(que)
+    if dist[x] < dis :
+        continue
+    dist[x] = dis
+
+    for nx, nd in graph[x] :
+        n_cost = dis + nd
+        if dist[nx] > n_cost:
+            dist[nx] = n_cost
+            heapq.heappush(que, (n_cost, nx))
+
+for i in dist[1:] :
+    print(i if i != 1e12 else "INF")
