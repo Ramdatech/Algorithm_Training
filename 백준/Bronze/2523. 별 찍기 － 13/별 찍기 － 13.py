017499b786fd705ff65b9a790f21edefbd928553
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-05-22 20:54:38 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.py
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.py
@@ -1,8 +1,3 @@
-import sys
-t = sys.stdin.readline
-n = int(t())
+n = int(input())
 for i in range(1,2*n):
-    if i > n:
-        print("*"*(2*n-i))
-    else :
-        print("*"*i)
+    print("*"*(2*n-i) if i > n else "*"*i)
