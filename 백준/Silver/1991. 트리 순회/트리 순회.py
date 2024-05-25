f48bf3f55bb7d9fac58be6bc84aaddefbbb1c805
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-05-26 01:29:51 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -3,23 +3,17 @@ t = sys.stdin.readline
 n = int(t())
 dp = {}
 for _ in range(n):
-    a, b, c = t().strip().split()
-    dp[a] = (b, c)
-res = ["", "", ""]
-def rec(cur, text, num) :
-    global pre
-    if num == 0 :
-        res[num] += cur
-    if dp[cur][0] != "." :
-        rec(dp[cur][0], text, num)
-    if num == 1 :
-        res[num] += cur
-    if dp[cur][1] != "." :
-        rec(dp[cur][1], text, num)
-    if num == 2 :
-        res[num] += cur
-
-for i in range(3):
-    rec("A", "", i)
-
+    a, *b = t().strip().split()
+    dp[a] = b
+res = [""]*3
+def r(n, t):
+    res[n] += t
+def rec(cur) :
+    r(0, cur)
+    o, p = [x!="." for x in dp[cur]]
+    if o : rec(dp[cur][0])
+    r(1, cur)
+    if p : rec(dp[cur][1])
+    r(2, cur)
+rec("A")
 print(*res, sep="\n")
