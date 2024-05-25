9b401ea927b05f9e7efc9377446118cc1cc5946d
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-05-26 00:48:44 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -4,7 +4,6 @@ n = int(t())
 ls = [list(map(int, t().strip().split())) for _ in range(n)]
 vst = [[[0, 0, 0] for _ in range(n)] for _ in range(n)]
 vst[0][1] = [1, 0, 0]
-dirs = [(0, -1), (-1, 0), (-1, -1)]
 for i in range(1, 2*n-1) :
     for x, y in [(j, i-j) for j in range(i+1) if 0<=j<n and 0<=i-j<n] :
         if ls[x][y] == 1 :
@@ -14,6 +13,5 @@ for i in range(1, 2*n-1) :
         if 0<=x-1 < n and 0<=y < n and ls[x-1][y] == 0 :
             vst[x][y][1] += vst[x-1][y][1] + vst[x-1][y][2]
         if 0<=x-1 < n and 0<=y-1 < n and ls[x-1][y-1] == 0 and ls[x-1][y] == 0 and ls[x][y-1] == 0 :
-            vst[x][y][2] += vst[x-1][y-1][0] + vst[x-1][y-1][1] + vst[x-1][y-1][2]
-
+            vst[x][y][2] += sum(vst[x-1][y-1])
 print(sum(vst[-1][-1]))
