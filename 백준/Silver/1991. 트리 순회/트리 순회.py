3bfcf3daf36ef6d25bc7e5c1a547195eb183c846
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-05-26 01:34:13 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,19 +1,15 @@
-import sys
-t = sys.stdin.readline
-n = int(t())
-dp = {}
+n = int(input())
+d = {}
 for _ in range(n):
-    a, *b = t().strip().split()
-    dp[a] = b
-res = [""]*3
-def r(n, t):
-    res[n] += t
-def rec(cur) :
-    r(0, cur)
-    o, p = [x!="." for x in dp[cur]]
-    if o : rec(dp[cur][0])
-    r(1, cur)
-    if p : rec(dp[cur][1])
-    r(2, cur)
-rec("A")
-print(*res, sep="\n")
+    a, *b = input().split()
+    d[a] = b
+z = [""]*3
+def Z(c) :
+    z[0]+=c
+    o, p = [x!="." for x in d[c]]
+    if o : Z(d[c][0])
+    z[1]+=c
+    if p : Z(d[c][1])
+    z[2]+=c
+Z("A")
+print(*z, sep="\n")
