7beaf1d77c062b4eaffa605ff0f92ad7f728cc74
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-05-25 21:59:06 +0900

    [Gold V] Title: 내려가기, Time: 576 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -4,12 +4,11 @@ n = int(t())
 tmp = [[x, x] for x in list(map(int, t().strip().split()))]
 for _ in range(0, n-1) :
     a = list(map(int, t().strip().split()))
-    s = [[z for z in [j-1, j, j+1] if 0 <= z < 3] for j in range(3)]
-    s = [[[tmp[j][0] for j in i] for i in s], [[tmp[j][1] for j in i] for i in s]]
-    tmp = [[a[t] + max(s[0][t]), a[t] + min(s[1][t])] for t in range(3)]
+    tx = []
+    for i in [[0, 1], [0, 1, 2], [1, 2]] :
+        x, y = zip(*[tmp[j] for j in i])
+        tx.append((max(x), min(y)))
+    tmp = [[a[t] + tx[t][0], a[t] + tx[t][1]] for t in range(3)]
 
-mx, mn = -1e12, 1e12
-for x, y in tmp:
-    mx = max(mx, x)
-    mn = min(mn, y)
-print(mx, mn)
+x, y = zip(*tmp)
+print(max(x), min(y))
