deb632a743cf433b48fbfea5bb8ebab0e51ee31e
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-05-26 16:33:08 +0900

    [Gold V] Title: A와 B, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -2,13 +2,13 @@ import sys
 t = sys.stdin.readline
 n = list(t().strip())
 m = list(t().strip())
-for _ in range(len(m)):
-    if m == n :
-        print(1)
-        break
-    if m[-1] == "B" :
-        m = m[:-1][::-1]
-    elif m[-1] == "A" :
-        m.pop()
-else :
-    print(0)
+def w(n, m):
+    for _ in range(len(m)):
+        if m == n :
+            return True
+        if m[-1] == "B" :
+            m = m[:-1][::-1]
+        elif m[-1] == "A" :
+            m.pop()
+    return False
+print(1 if w(n, m) else 0)
