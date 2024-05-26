f2e2abb92000b522750dc572abe5c7209ee48b8b
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-05-26 15:24:04 +0900

    [Gold IV] Title: 좋다, Time: 2504 ms, Memory: 33184 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -6,9 +6,14 @@ res = 0
 for idx, goal in enumerate(ls):
     tmplist = ls[:idx] + ls[idx+1:]
     for j in range(n-1):
-        a, b = sorted([goal-tmplist[j], tmplist[j]])
-        p1 = bisect.bisect_left(tmplist, a, 0, n-1)
-        p2 = bisect.bisect_right(tmplist, b, 0, n-1)-1
+        a = goal - tmplist[j]
+        b = tmplist[j]
+        if a < b :
+            p1 = bisect.bisect_left(tmplist, a, 0, n - 1)
+            p2 = j
+        else :
+            p1 = bisect.bisect_right(tmplist, b, 0, n-1)-1
+            p2 = j
         if p1 != p2 and tmplist[p1] + tmplist[p2] == goal :
             res += 1
             break
