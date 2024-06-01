4417af0f198157e99607982bbae7a0a1901859b4
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-06-01 14:49:48 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 6516 ms, Memory: 152404 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -17,11 +17,12 @@ while que :
 
     for dx, dy in dirs :
         nx, ny = x+dx, y+dy
-        if 0<=nx<n and 0<=ny<m and ls[x][y] == 0 and not msk[nx][ny][b] :
-            msk[nx][ny][b] = True
-            que.append((nx, ny, b, dist+1))
-        if b == 0 and 0<=nx<n and 0<=ny<m and ls[x][y] == 1 and not msk[nx][ny][b] :
-            msk[nx][ny][1] = True
-            que.append((nx, ny, 1, dist+1))
+        if 0<=nx<n and 0<=ny<m :
+            if ls[x][y] == 0 and not msk[nx][ny][b] :
+                msk[nx][ny][b] = True
+                que.append((nx, ny, b, dist+1))
+            elif b == 0 and not msk[nx][ny][b] :
+                msk[nx][ny][1] = True
+                que.append((nx, ny, 1, dist+1))
 
 print(-1)
