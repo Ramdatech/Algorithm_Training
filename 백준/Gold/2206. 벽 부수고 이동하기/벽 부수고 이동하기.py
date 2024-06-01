5b0176c074d287c5283d7527bb0c590925093732
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-06-01 14:48:19 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 7200 ms, Memory: 151340 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+t = sys.stdin.readline
+n, m = map(int, t().strip().split())
+ls = [list(map(int, list(t().strip()))) for _ in range(n)]
+dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+msk = [[[False, False] for _ in range(m)] for _ in range(n)]
+
+que = deque([(0, 0, 0, 1)])
+msk[0][0][0] = True
+
+while que :
+    x, y, b, dist = que.popleft()
+    if x == n - 1 and y == m - 1:
+        print(dist)
+        sys.exit(0)
+
+    for dx, dy in dirs :
+        nx, ny = x+dx, y+dy
+        if 0<=nx<n and 0<=ny<m and ls[x][y] == 0 and not msk[nx][ny][b] :
+            msk[nx][ny][b] = True
+            que.append((nx, ny, b, dist+1))
+        if b == 0 and 0<=nx<n and 0<=ny<m and ls[x][y] == 1 and not msk[nx][ny][b] :
+            msk[nx][ny][1] = True
+            que.append((nx, ny, 1, dist+1))
+
+print(-1)
