d8ba64aa70fc7a1cd9216e6c4b8083c8640750ff
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-06-02 13:30:31 +0900

    [Gold I] Title: 전구를 켜라, Time: 1260 ms, Memory: 184168 KB -BaekjoonHub

--- 백준/Gold/2423. 전구를 켜라/전구를 켜라.py
+++ 백준/Gold/2423. 전구를 켜라/전구를 켜라.py
@@ -3,45 +3,32 @@ t = sys.stdin.readline
 n, m = map(int, t().split())
 ls = [list(t().strip()) for _ in range(n)]
 n, m = n+1, m+1
-f_ls = [[] for _ in range(n*m)]
-r_ls = [[] for _ in range(n*m)]
+msk = [[[] for _ in range(n*m)] for _ in range(2)]
 for i in range(n-1):
     for j in range(m-1):
         a, b, c, d = i*m+j, (i+1)*m+j+1, i*m+j+1, (i+1)*m+j
-        if ls[i][j] == '\\':
-            if a < n*m and b < n*m:
-                f_ls[a].append((b, 1))
-                f_ls[b].append((a, 1))
-            if c < n*m and d < n*m:
-                r_ls[c].append((d, 1))
-                r_ls[d].append((c, 1))
-        else :
-            if c < n*m and d < n*m:
-                f_ls[c].append((d, 1))
-                f_ls[d].append((c, 1))
-            if a < n*m and b < n*m:
-                r_ls[a].append((b, 1))
-                r_ls[b].append((a, 1))
+        addr = 0 if ls[i][j] == '\\' else 1
+        if a < n*m and b < n*m:
+            msk[addr][a].append((b, 1))
+            msk[addr][b].append((a, 1))
+        if c < n*m and d < n*m:
+            msk[addr^1][c].append((d, 1))
+            msk[addr^1][d].append((c, 1))
 
 que = []
-heapq.heappush(que, (0, 0, 0))
-dist = [(1e9, 1e9)]*(n*m)
-dist[0] = (0, 0)
+heapq.heappush(que, (0, 0))
+dist = [0] + [1e9]*(n*m-1)
 while que :
-    cnt, cost, cur = heapq.heappop(que)
+    cnt, cur = heapq.heappop(que)
+    cur *= -1
     if cur == n*m-1:
         print(cnt)
         break
-    
-    for nxt, ncost in f_ls[cur]:
-        cn, co = dist[nxt]
-        if cn > cnt:
-            dist[nxt] = (cnt, cost+ncost)
-            heapq.heappush(que, (cnt, cost+ncost, nxt))
-    for nxt, ncost in r_ls[cur]:
-        cn, co = dist[nxt]
-        if cn > cnt+1:
-            dist[nxt] = (cnt+1, cost+ncost)
-            heapq.heappush(que, (cnt+1, cost+ncost, nxt))
+
+    for tmp in range(2) :
+        for nxt, ncost in msk[tmp][cur]:
+            if dist[nxt] > cnt + tmp:
+                dist[nxt] = cnt + tmp
+                heapq.heappush(que, (cnt+tmp, -nxt))
 else :
     print("NO SOLUTION")
