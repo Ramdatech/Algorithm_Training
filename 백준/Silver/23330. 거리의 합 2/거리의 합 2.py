f6dd8e57a5a67f79c4e94401d107fc0e584bb0ad
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-06-09 15:58:46 +0900

    [Silver III] Title: 거리의 합 2, Time: 636 ms, Memory: 90152 KB -BaekjoonHub

--- 백준/Silver/23330. 거리의 합 2/거리의 합 2.py
+++ 백준/Silver/23330. 거리의 합 2/거리의 합 2.py
@@ -2,7 +2,4 @@ import sys
 t = sys.stdin.readline
 n = int(t())
 ls = sorted(list(map(int, t().strip().split())))
-res = 0
-for i in range(1, n) :
-    res += (ls[i]-ls[i-1])*abs((n-i)**2-n*(n-i))
-print(res*2)
+print(sum([(ls[i]-ls[i-1])*abs((n-i)**2-n*(n-i)) for i in range(1, n)])*2)
