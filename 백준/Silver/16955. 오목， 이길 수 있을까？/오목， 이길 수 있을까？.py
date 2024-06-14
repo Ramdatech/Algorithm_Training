1b58ef73864c0df30516371672403b5452707b6f
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-06-14 10:08:20 +0900

    [Silver IV] Title: 오목, 이길 수 있을까?, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.py
+++ 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.py
@@ -6,14 +6,16 @@ msk = ["."] + ["X"] * 4
 def f(ls):    
     for i in range(10):
         for j in range(10):
-            if 4<=i+4<=9 and 0<=j-4<=5 and sorted([ls[i+x][j-x] for x in range(5)]) == msk:
-                return 1
-            if 4<=i+4<=9 and 4<=j+4<=9 and sorted([ls[i+x][j+x] for x in range(5)]) == msk :
-                return 1
-            if len(ls[i][j:j+5])==5 and sorted(ls[i][j:j+5]) == msk :
-                return 1
-            if 4<=i+4<=9 and sorted([ls[i+x][j] for x in range(5)]) == msk :
-                return 1
+            for t1, t2 in [(0, 1), (1, 0), (1, -1), (1, 1)]:
+                tmp = []
+                for x in range(5):
+                    if not (0 <= i + x * t1 < 10 and 0 <= j + x * t2 < 10):
+                        break
+                    if ls[i + x * t1][j + x * t2] == "O":
+                        break
+                    tmp.append(ls[i + x * t1][j + x * t2])
+                if len(tmp) == 5 and sorted(tmp) == msk:
+                    return 1
     return 0
 
 print(f(lst))
