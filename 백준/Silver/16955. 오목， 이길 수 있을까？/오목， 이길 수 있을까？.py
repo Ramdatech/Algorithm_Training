d77cf267391026980a1f4ed0fda3930f854d4ae1
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-06-14 10:14:36 +0900

    [Silver IV] Title: 오목, 이길 수 있을까?, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.py
+++ 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.py
@@ -1,20 +1,20 @@
 import sys
 t = sys.stdin.readline
 lst = [list(t().strip()) for _ in range(10)]
-msk = ["."] + ["X"] * 4
+msk = {"X": 0, ".": 1}
 
 def f(ls):    
     for i in range(10):
         for j in range(10):
             for t1, t2 in [(0, 1), (1, 0), (1, -1), (1, 1)]:
-                tmp = []
+                L = [0, 0]
+                if not (0<=i+4*t1<10 and 0<=j+4*t2<10) :
+                    continue
                 for x in range(5):
-                    if not (0 <= i + x * t1 < 10 and 0 <= j + x * t2 < 10):
+                    if ls[i+x*t1][j+x*t2] == "O":
                         break
-                    if ls[i + x * t1][j + x * t2] == "O":
-                        break
-                    tmp.append(ls[i + x * t1][j + x * t2])
-                if len(tmp) == 5 and sorted(tmp) == msk:
+                    L[msk[ls[i+x*t1][j+x*t2]]] += 1
+                if sum(L) == 5 and L[0] == 4:
                     return 1
     return 0
 
