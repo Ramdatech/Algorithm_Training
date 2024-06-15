dc26f9234beba1c38e8678ec85d9a09b59814fc1
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-06-15 13:52:27 +0900

    [Silver IV] Title: 행운을 빌어요, Time: 204 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/31713. 행운을 빌어요/행운을 빌어요.py
+++ 백준/Silver/31713. 행운을 빌어요/행운을 빌어요.py
@@ -3,29 +3,24 @@ t = sys.stdin.readline
 n = int(t())
 ls = [list(map(int, t().split())) for _ in range(n)]
 
-def f(lst):
-    res = []
+def f(x, y):
+    ans = float('inf')
 
-    for x, y in lst:
-        mn = float('inf')
+    for dx in range(500):
+        nx = x+dx
+        mn, mx = 3*nx, 4*nx
 
-        for ex in range(1001):
-            total = x + ex
-            mn_l = 3 * total
-            mx_l = 4 * total
+        if mn <= y <= mx:
+            ans = min(ans, dx)
+        elif y < mn:
+            tmp = mn-y
+            ans = min(ans, dx+tmp)
+        elif y > mx:
+            tmp = (y - mx + 2) // 3
+            ny = y+tmp
+            if mn <= ny <= mx:
+                ans = min(ans, dx+tmp)
+    return ans
 
-            if mn_l <= y <= mx_l:
-                mn = min(mn, ex)
-            elif y < mn_l:
-                a_lv = mn_l - y
-                mn = min(mn, ex + a_lv)
-            elif y > mx_l:
-                tmp_lv = (y - mx_l + 2) // 3
-                n_lv = y + tmp_lv
-                if 3 * total <= n_lv <= 4 * total:
-                    mn = min(mn, ex + tmp_lv)
-        res.append(mn)
-    return res
-
-for a in f(ls):
-    print(a)
+for x,y in ls:
+    print(f(x,y))
