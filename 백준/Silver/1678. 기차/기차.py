5d615418bbeb318cd156d1a0e24750d8898fd6fc
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-07-15 09:33:49 +0900

    [Silver III] Title: 기차, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1678. 기차/기차.py
+++ 백준/Silver/1678. 기차/기차.py
@@ -5,10 +5,9 @@ for _ in range(T):
     tn, *tms = input().split()
     tms = list(map(int, tms[:-1]))
     for t in tms :
-        if t-M < stt[0] and t >= M :
-            stt = [t-M, [t, tn]]
-        if (t+60)-M < stt[0] and M <= t+60 :
-            stt = [(t+60)-M, [t, tn]]
+        for dt in [0, 60]:
+            if t+dt-M < stt[0] and M <= t+dt :
+                stt = [t+dt-M, [t, tn]]
         tab.append([t, tn])
 tab.sort()
 print(tab[(tab.index(stt[1])+N)%len(tab)-1][1])
