0c006af5628860c72b2b9592d01200d14e473a38
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-07-15 09:32:55 +0900

    [Silver III] Title: 기차, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1678. 기차/기차.py
+++ 백준/Silver/1678. 기차/기차.py
@@ -1,10 +1,14 @@
-import bisect
 T, M, N = map(int, input().split())
 tab = []
+stt = [1e12, [0, 0]]
 for _ in range(T):
     tn, *tms = input().split()
     tms = list(map(int, tms[:-1]))
     for t in tms :
+        if t-M < stt[0] and t >= M :
+            stt = [t-M, [t, tn]]
+        if (t+60)-M < stt[0] and M <= t+60 :
+            stt = [(t+60)-M, [t, tn]]
         tab.append([t, tn])
 tab.sort()
-print(tab[(bisect.bisect_left(list(zip(*tab))[0],M)+N)%len(tab)-1][1])
+print(tab[(tab.index(stt[1])+N)%len(tab)-1][1])
