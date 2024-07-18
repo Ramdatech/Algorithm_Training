59a2c36cb2bd4acb1c833b5fb75f74ba1e814d5d
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-07-18 10:55:38 +0900

    [Silver III] Title: 돌아온 똥게임, Time: 120 ms, Memory: 35756 KB -BaekjoonHub

--- 백준/Silver/30701. 돌아온 똥게임/돌아온 똥게임.py
+++ 백준/Silver/30701. 돌아온 똥게임/돌아온 똥게임.py
@@ -1,20 +1,23 @@
 import sys
 t = sys.stdin.readline
 n, m = map(int, t().split())
-M = [[], []]
-AS=BS=0
-for _ in range(n) :
-    a, b = map(int, t().split())
-    if a-1 or m <= b : M[a-1]+=[b]
-    else : m += b
-(A, AL), (B, BL) = [[sorted(x), len(x)] for x in M]
-while 1 :
-    while AS < AL and m > A[AS]:
-        m += A[AS]
-        AS += 1
-    while AS < AL and BS < BL and m <= A[AS]:
-        m *= B[BS]
-        BS += 1
-    if AS == AL or A[AS] >= m :
-        break
-print(n-(AL-AS))
+def f(n,m) :
+    M = [[], []]
+    AS=BS=0
+    for _ in range(n) :
+        a, b = map(int, t().split())
+        if a-1 or m<=b : M[a-1]+=[b]
+        else : m+=b
+    A, B = [sorted(x) for x in M]
+    AL, BL = len(A), len(B)
+    while 1 :
+        while AS < AL and m > A[AS]:
+            m += A[AS]
+            AS += 1
+        while AS < AL and BS < BL and m <= A[AS]:
+            m *= B[BS]
+            BS += 1
+        if AS == AL or A[AS] >= m :
+            break
+    print(n-AL+AS)
+f(n,m)
