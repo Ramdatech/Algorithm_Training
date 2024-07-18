25965d2cb0fa7b648e0f5bf4bf2949629bcf125d
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-07-18 11:07:32 +0900

    [Silver III] Title: 돌아온 똥게임, Time: 120 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Silver/30701. 돌아온 똥게임/돌아온 똥게임.py
+++ 백준/Silver/30701. 돌아온 똥게임/돌아온 똥게임.py
@@ -1,22 +1,18 @@
 import sys
 t = sys.stdin.readline
 def f(n,m) :
-    M = [[], []]
+    A, B = M = [[], []]
     AS=BS=0
     for _ in range(n) :
         a, b = map(int, t().split())
         if a-1 or m<=b : M[a-1]+=[b]
         else : m+=b
-    A, B = [sorted(x) for x in M]
-    AL, BL = len(A), len(B)
+    A.sort(); B.sort()
+    AL, BL =len(A),len(B)
     while 1 :
         while AS < AL and m > A[AS]:
-            m += A[AS]
-            AS += 1
+            m += A[AS]; AS += 1
         while AS < AL and BS < BL and m <= A[AS]:
-            m *= B[BS]
-            BS += 1
-        if AS == AL or A[AS] >= m :
-            break
-    print(n-AL+AS)
-f(*map(int, t().split()))
+            m *= B[BS]; BS += 1
+        if AS == AL or A[AS] >= m : return n-AL+AS
+print(f(*map(int, t().split())))
