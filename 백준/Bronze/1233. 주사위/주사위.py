84370180aaa773927ea37390dcd00133d1346375
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-07-27 16:19:38 +0900

    [Bronze II] Title: 주사위, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1233. 주사위/주사위.py
+++ 백준/Bronze/1233. 주사위/주사위.py
@@ -1,9 +1,9 @@
 def f(a, b, c):
     ls = [[0, i] for i in range(a+b+c+1)]
-    for i in range(1, a+1):
-        for j in range(1, b+1):
-            for k in range(1, c+1):
-                ls[i+j+k][0] -= 1
+    for i in range(a):
+        for j in range(b):
+            for k in range(c):
+                ls[i+j+k+3][0] -= 1
     ls.sort()
     return ls[0][1]
 print(f(*map(int, input().split())))
