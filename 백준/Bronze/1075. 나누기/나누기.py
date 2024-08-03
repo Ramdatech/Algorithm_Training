cecfcc0b2593b6e1479b0c02d8efcc38c166593c
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-08-03 17:53:44 +0900

    [Bronze II] Title: 나누기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1075. 나누기/나누기.py
+++ 백준/Bronze/1075. 나누기/나누기.py
@@ -1,2 +1,2 @@
-n, d = int(input())//100*100, int(input())
-print("00" if n%d == 0 else str(d-n%d).zfill(2))
+n,d = int(input())//100*100, int(input())
+print("00" if n%d==0 else str(d-n%d).zfill(2))
