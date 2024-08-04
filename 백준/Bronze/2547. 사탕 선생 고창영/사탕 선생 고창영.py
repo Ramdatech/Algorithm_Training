c637b625a38b1713c98d79c4ffa9cd4d9a1c7239
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-08-04 14:10:00 +0900

    [Bronze III] Title: 사탕 선생 고창영, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2547. 사탕 선생 고창영/사탕 선생 고창영.py
+++ 백준/Bronze/2547. 사탕 선생 고창영/사탕 선생 고창영.py
@@ -1,11 +1,10 @@
 import sys
 t = sys.stdin.readline
 n = int(t())
-t()
 for _ in range(n):
+    t()
     m = int(t())
     if sum(int(t()) for x in range(m)) % m == 0 :
         print("YES")
     else :
         print("NO")
-    t()
