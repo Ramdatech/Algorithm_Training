7d3f3bf869da72706fa8d146e2c2572d56d9bb00
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-08-06 10:40:17 +0900

    [Silver IV] Title: 토너먼트, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.py
+++ 백준/Silver/1057. 토너먼트/토너먼트.py
@@ -1,10 +1,4 @@
-n, *ls = map(int, input().split())
-a, b = sorted(ls)
-mx = 0
-while 1 :
-    mx += 1
-    if a == b :
-        break
-    a = (a+1)//2
-    b = (b+1)//2
-print(mx-1)
+_, a, b = map(int, input().split())
+mx=0
+while a!=b: a,b=(a+1)//2,(b+1)//2; mx+=1
+print(mx)
