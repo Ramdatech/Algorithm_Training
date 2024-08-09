f6ef2789a75e65e018edbe0b7575d4590f6f0bea
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-08-09 15:31:30 +0900

    [Silver IV] Title: 볼링공 찾아주기, Time: 468 ms, Memory: 156336 KB -BaekjoonHub

--- 백준/Silver/28446. 볼링공 찾아주기/볼링공 찾아주기.py
+++ 백준/Silver/28446. 볼링공 찾아주기/볼링공 찾아주기.py
@@ -3,7 +3,7 @@ t = sys.stdin.readline
 n, d = int(t()), {}
 for _ in range(n):
     a, b, *c = map(int, t().split())
-    if a == 1 :
+    if c :
         d[c[0]] = b
     else :
         print(d[b])
