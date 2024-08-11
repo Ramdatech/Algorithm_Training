9cba0ef8563079b1d79a0b5061330f694dfcbfeb
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-08-11 12:38:03 +0900

    [Bronze II] Title: 애너그램 거리, Time: 360 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3778. 애너그램 거리/애너그램 거리.py
+++ 백준/Bronze/3778. 애너그램 거리/애너그램 거리.py
@@ -2,7 +2,7 @@ import sys
 t = sys.stdin.readline
 N, n = int(t()), 26
 for i in range(N):
-    cnt1, cnt2 = [0]*n, [0]*n
-    for c in t().strip(): cnt1[ord(c)-97]+=1
-    for c in t().strip(): cnt2[ord(c)-97]+=1
-    print(f"Case #{i+1}: {sum(abs(x-y) for x,y in zip(cnt1, cnt2))}")
+    cnt = [0]*n*2
+    for c in t().strip(): cnt[ord(c)-97]+=1
+    for c in t().strip(): cnt[ord(c)-97+n]+=1
+    print(f"Case #{i+1}: {sum(abs(cnt[a]-cnt[a+n]) for a in range(n))}")
