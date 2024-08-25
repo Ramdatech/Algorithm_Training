2207338b1e57c976fdf59f98e87394ad7ba8a627
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-08-25 21:04:28 +0900

    [Bronze V] Title: 라면 공식, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/30007. 라면 공식/라면 공식.py
+++ 백준/Bronze/30007. 라면 공식/라면 공식.py
@@ -1,3 +1,5 @@
-for _ in range(int(input())):
-    a,c,b = map(int, input().split())
+import sys
+t = sys.stdin.readline
+for _ in range(int(t())):
+    a,c,b = map(int, t().split())
     print(a*(b-1)+c)
