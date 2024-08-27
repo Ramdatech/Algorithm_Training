7d4b883657f7a9758854d2d7614a504847dc784e
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-08-27 11:13:35 +0900

    [Silver V] Title: 번데기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15721. 번데기/번데기.py
+++ 백준/Silver/15721. 번데기/번데기.py
@@ -1,11 +1,8 @@
 def f(a, b, c):
     tot, res = 0, 0
     for i in range(2, 100000):
-        for j in [0, 1]*2+[0]*i+[1]*i :
-            if j == c :
-                res += 1
-            if res == b :
-                return tot%a
+        for j in [0,1]*2+[0]*i+[1]*i :
+            if j == c : res += 1
+            if res == b : return tot%a
             tot += 1
-
 print(f(*map(int, [input(), input(), input()])))
