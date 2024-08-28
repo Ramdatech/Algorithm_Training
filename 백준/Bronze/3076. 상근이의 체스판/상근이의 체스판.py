4fcd529439baa0d63ec2c2fac8d7231e5b66e6bd
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-08-29 00:53:10 +0900

    [Bronze II] Title: 상근이의 체스판, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3076. 상근이의 체스판/상근이의 체스판.py
+++ 백준/Bronze/3076. 상근이의 체스판/상근이의 체스판.py
@@ -1,3 +1,3 @@
 R, C, a, b = map(int, input().split()+input().split())
 for r in range(R*a) :
-    print(*["X."[(r//a+c//b)%2] for c in range(C*b)], sep="")
+    print(''.join("X."[(r//a+c//b)%2] for c in range(C*b)))
