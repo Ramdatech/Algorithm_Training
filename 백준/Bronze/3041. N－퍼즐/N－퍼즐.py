1e1838bb79d2c1d986dd1dbf4a5ab0b27f347d3c
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-08-29 13:54:23 +0900

    [Bronze I] Title: N-퍼즐, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3041. N－퍼즐/N－퍼즐.py
+++ 백준/Bronze/3041. N－퍼즐/N－퍼즐.py
@@ -1,2 +1,2 @@
-def f(a) : return a//4, a%4
-print(sum(sum(sum((abs(x-y) for x, y in zip([i,j],f(ord(c)-65)))) for j, c in enumerate(input()) if c != ".") for i in range(4)))
+def f(a, i, j) : return abs(a//4-i)+abs(a%4-j)
+print(sum(sum(f(ord(c)-65,i,j) for j, c in enumerate(input()) if c != ".") for i in range(4)))
