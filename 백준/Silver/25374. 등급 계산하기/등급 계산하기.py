a556ba1a83288ebb40c7880369226cc9edd3c352
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-08-31 02:22:55 +0900

    [Silver V] Title: 등급 계산하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25374. 등급 계산하기/등급 계산하기.py
+++ 백준/Silver/25374. 등급 계산하기/등급 계산하기.py
@@ -1,15 +1,15 @@
 n = int(input())
-scores = sorted(list(map(int, input().split())), reverse=True)
-msk = [0.04, 0.11, 0.23, 0.40, 0.60, 0.77, 0.89, 0.96, 1.00]
+scr = sorted(list(map(int, input().split())), reverse=True)
+msk = [4, 11, 23, 40, 60, 77, 89, 96, 100]
 cnt, cur = [0] * 9, 0
 for i, chk in enumerate(msk):
     if cur >= n:
         break
-    mx = int(chk * n)
+    mx = int(chk*n/100)
     while cur < n and cur < mx:
         cnt[i] += 1
         cur += 1
-    while cur < n and scores[cur] == scores[cur - 1]:
+    while cur < n and scr[cur] == scr[cur - 1]:
         cnt[i] += 1
         cur += 1
 print(*cnt, sep="\n")
