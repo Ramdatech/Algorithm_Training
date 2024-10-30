a97ceb00b5e6552d7cc7c7597468e971072799f2
KD Park <62131378+Ramdatech@users.noreply.github.com>
2024-10-30 09:46:36 +0900

    [Silver IV] Title: 스도쿠 채점, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9291. 스도쿠 채점/스도쿠 채점.py
+++ 백준/Silver/9291. 스도쿠 채점/스도쿠 채점.py
@@ -1,18 +1,15 @@
 import sys
 t = sys.stdin.readline
 
+n, idx = int(t()), 0
+arr = [[] for _ in range(n)]
 def chk(lst):
     for l in lst :
-        if len(set(l)) != 9:
-            return False
+        if len(set(l)) != 9: return False
     return True
 
-n = int(t())
-idx = 0
-arr = [[] for _ in range(n)]
 for c in range(n*10-1) :
-    tmp = t().strip()
-    if tmp != "":
+    if (tmp := t().strip()) != "":
         arr[idx].append(list(map(int, tmp.split())))
     else:
         idx += 1
@@ -21,7 +18,7 @@ for i, lst in enumerate(arr):
     q = f"Case {i+1}:"
     if chk(lst):
         if chk(list(zip(*lst))):
-            if chk([lst[i][j:j+3] + lst[i+1][j:j+3] + lst[i+2][j:j+3] for i in range(0, 9, 3) for j in range(0, 9, 3)]):
+            if chk([sum([lst[i+m][j:j+3] for m in range(3)], []) for i in range(0, 9, 3) for j in range(0, 9, 3)]):
                 print(q, "CORRECT")
                 continue
     print(q, "INCORRECT")
